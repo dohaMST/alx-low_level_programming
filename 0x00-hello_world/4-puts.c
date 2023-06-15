464062ac5f3070072d5610437c608ffd677cb763
dohaMST <mastourdoha.dev001@gmail.com>
2023-06-15 00:16:45 -0700

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /*
  * main - this program will print a sentence using puts function
- * Return : 0 (success)
+ * Return: 0 (success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
