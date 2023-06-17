666fb4a80e7a78de8b076e2d400f72d7efe97148
dohaMST <mastourdoha.dev001@gmail.com>
2023-06-16 18:36:34 -0700

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar((i / 10) + 48);
 			putchar((i % 10) + 48);
