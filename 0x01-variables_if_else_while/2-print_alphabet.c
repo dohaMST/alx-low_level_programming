c8109437d1d4d08557196f37c727f2e4f39f9c40
dohaMST <mastourdoha.dev001@gmail.com>
2023-06-16 17:06:15 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this program will print the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
