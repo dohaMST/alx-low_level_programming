22fef291b0a15b4180b04444e193686ca901d2f6
dohaMST <mastourdoha.dev001@gmail.com>
2023-06-29 04:42:56 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,10 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
-			return (15);
+			return (s1[i] - s2[i]);
 		else if (s1[i] < s2[i])
-			return (-15);
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	if (s1[i] != '\0' && s2[i] != '\0')
-		return (15);
-	else if (s1[i] != '\0' && s2[i] != '\0')
-		return (-15);
 	return (0);
 }
