d1251807a0270a2d29c71795c206731bc078131c
dohaMST <mastourdoha.dev001@gmail.com>
2023-06-29 18:26:06 -0700

    task11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,6 +6,7 @@
  * @n2: number 2
  * @r: buffer that the function will use to store the result
  * @size_r: is the buffer size
+ * Return: string
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
