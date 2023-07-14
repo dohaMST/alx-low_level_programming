42e4ee5bc76beaf53c68ce26de68e968f194144d
dohaMST <mastourdoha.dev001@gmail.com>
2023-07-13 18:11:01 -0700

    task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,83 +1,102 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * _string - prints a string
- * @string: the string we gonna use
- */
-
-void _string(const char *string)
-{
-	int a = 0;
-
-	while (string[a])
-	{
-		_putchar(string[a]);
-		a++;
-	}
-}
-
-/**
- * ecrire_num - prints a number
- * @num: the number we gonna use
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-void ecrire_num(unsigned long int num)
+int is_digit(char *s)
 {
-	unsigned long int div = 1, a = 0, result;
+	int i = 0;
 
-	while (num / div > 9)
+	while (s[i])
 	{
-		a++;
-		div *= 10;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
-	for (; div >= 1; num %= div, div /= 10)
-	{
-		result = num / div;
-		_putchar('0' + result);
-	}
+	return (1);
 }
 
 /**
- * _atoi - convert a string to number
- * @s: the string we gonna use
- * Return: number
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	int x = 1;
-	unsigned long int result = 0, a, b;
+	int i = 0;
 
-	for (a = 0; !(s[a] >= 48 && s[a] <= 57); a++)
+	while (s[i] != '\0')
 	{
-		if (s[a] == '-')
-			x *= -1;
+		i++;
 	}
-	for (b = a; s[b] >= 48 && s[b] <= 57; b++)
-	{
-		result *= 10;
-		result += (s[b] - 48);
-	}
-	return (x * result);
+	return (i);
 }
 
 /**
- * main - print multiplication
- * @argc: size of array
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ *
+ * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	if (argc != 3)
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		_string("Error ");
-		exit(98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	ecrire_num(_atoi(argv[1]) * _atoi(argv[2]));
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
+	free(result);
 	return (0);
 }
+
