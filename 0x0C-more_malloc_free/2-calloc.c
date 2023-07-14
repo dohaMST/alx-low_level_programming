c248ccab473ae0486c9bfd6feeb21277654af6ad
dohaMST <mastourdoha.dev001@gmail.com>
2023-07-13 19:05:21 -0700

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * *_set_memoire - make a memory with a fix bytes
+ * @str: memory area
+ * @copy: char to copy
+ * @num:  number of times we gonna copy
+ * Return: pointer
+ */
+char *_set_memoire(char *str, char copy, unsigned int num)
+{
+	unsigned int a;
+
+	for (a = 0; a < num; a++)
+	{
+		str[a] = copy;
+	}
+
+	return (str);
+}
+
+/**
  * _calloc - a function that allocates memory for an array, using malloc.
  * @size: size of bytes
  * @nmemb: number of members in the array
@@ -10,4 +29,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *arr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+	_set_memoire(arr, 0, nmemb * size);
+	return (arr);
+}
