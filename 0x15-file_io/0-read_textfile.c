ca79c3768dd49f358a689d13570ef9dde7e9b6b4
dohaMST <mastourdoha.dev001@gmail.com>
2023-08-07 15:48:28 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || !letters)
 		return (0);
-	fl = open(filename, O_READONLY);
+	fl = open(filename, O_RDONLY);
 	if (fl == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
