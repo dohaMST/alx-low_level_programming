228ba3b38529acedb75fcda86df9f390286381ef
dohaMST <mastourdoha.dev001@gmail.com>
2023-08-07 16:16:33 -0700

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: name of file
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fl, bytes, size;
+
+	if (!filename || !text_content)
+		return (-1);
+
+	if (text_content)
+	{
+		for (size = 0; text_content[size];)
+			size++;
+	}
+
+	fl = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes = write(fl, text_content, size);
+
+	if (fl == -1 || bytes == -1)
+		return (-1);
+	close(fl);
+	return (1);
+}
