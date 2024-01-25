094d00bd7926ade194714e6273117f02493af10d
dohaMST <mastourdoha.dev001@gmail.com>
2024-01-25 15:55:01 -0800

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - a function that prints all the elements of a list
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int c = 0;
+
+	if (h == NULL)
+		return (c);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+a readme file for 0x17-doubly_linked_lists prj
