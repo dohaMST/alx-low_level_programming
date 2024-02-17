0090bad7bc98b0c9d7893cba52e92192b5e9a37d
dohaMST <mastourdoha.dev001@gmail.com>
2024-02-16 17:56:42 -0800

    awdii

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,59 +1,62 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
- *
- * Return: Upon failure - 0.
- *         Otherwise - 1.
- */
+ * hash_table_set - function that creates a hash table.
+ * @ht: The size of the array.
+ * @key: The size of the array.
+ * @value: The size of the array.
+ * Return: newly created hash table.
+*/
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new, *current;
-	char *value_copy;
-	unsigned long int idx, i;
+	hash_node_t *item;
+	unsigned long int index;
 
-	if (!ht || !key || *key == '\0' || !value)
+	if (ht == NULL)
 		return (0);
 
-	idx = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[idx];
-	while (current)
-	{
-		if (strcmp(current->key, key) == 0)
-		{
-			free(current->value);
-			current->value = strdup(value);
-			if (!current->value)
-				return (0);
-			return (1);
-		}
-		current = current->next;
-	}
+	if (key == NULL || strlen(key) == 0)
+		return (0);
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = strdup(value);
-	if (!new->value)
-	{
-		free(new->value);
-		free(new->key);
-		free(new);
-		return (0);
-	}
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	index = key_index((unsigned char *)key, ht->size);
 
+	item = add_node(&(ht->array[index]), key, value);
+	if (!item)
+		return (0);
 	return (1);
+}
+/**
+ * add_node - .
+ * @head: head
+ * @key: string
+ * @value: string
+ * Return: Always cnt.
+ */
+
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *item;
+
+	item = *head;
+
+	while (item)
+	{
+		if (strcmp(item->key, key) == 0)
+		{
+			free(item->value);
+			item->value = strdup(value);
+			return (*head);
+		}
+		item = item->next;
+	}
+
+	item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+		return (NULL);
+
+	item->key = strdup(key);
+	item->value = strdup(value);
+	item->next = *head;
+	*head = item;
+	return (*head);
 }
